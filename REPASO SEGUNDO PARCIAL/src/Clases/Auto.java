3b341119b95e4242b13efd1e260d784ba7d23056
Walter <waloalvarez13@gmail.com>
2024-11-01 13:14:01 -0300

    Clase Auto

--- REPASO SEGUNDO PARCIAL/src/Clases/Auto.java
+++ REPASO SEGUNDO PARCIAL/src/Clases/Auto.java
@@ -1,4 +1,11 @@
 package Clases;
 
 public class Auto extends Concesionaria{
+    public Auto() {
+    }
+
+    @Override
+    public String toString() {
+        return "Auto{} " + super.toString();
+    }
 }
